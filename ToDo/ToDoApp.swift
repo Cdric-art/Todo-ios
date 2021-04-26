4fb17445c147fd5e62060fb91ad6ff2d20824dfc
Cdric_art <cdric.art@gmail.com>
2021-04-26 19:49:53 +0200

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Cédric Evrard on 26/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
