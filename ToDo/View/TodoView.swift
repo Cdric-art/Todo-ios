2cb5a203ff5c5b6808c504513a54cd6568662c5e
Cdric_art <cdric.art@gmail.com>
2021-04-30 19:15:26 +0200

    Feat: Navigation after Login success and View for Todo

---
+++ ToDo/View/TodoView.swift
@@ -0,0 +1,25 @@
+//
+//  TodoView.swift
+//  ToDo
+//
+//  Created by Cédric Evrard on 30/04/2021.
+//
+
+import SwiftUI
+
+struct TodoView: View {
+	@Binding var pseudo: String
+	
+    var body: some View {
+		NavigationView {
+			Text("Welcome \(pseudo)")
+		}
+		.navigationBarHidden(true)
+    }
+}
+
+struct TodoView_Previews: PreviewProvider {
+    static var previews: some View {
+		TodoView(pseudo: .constant("Demo"))
+    }
+}
